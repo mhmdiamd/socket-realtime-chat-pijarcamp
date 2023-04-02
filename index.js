62824a6a97980196044ffae2a6848a289b88e9dd
mhmdiamd <darmawanilham34@gmail.com>
2023-04-03 05:38:29 +0700

    add logout event

--- index.js
+++ index.js
@@ -26,47 +26,52 @@ io.on("connection", (socket) => {
         userId: userLoginId,
         socketId: socket.id
       })
-    }else {
+    } else {
       const getUserLogin = activeUsers.find(user => user.userId == userLoginId)
 
-      if(socket.id != getUserLogin.socketId){
+      if (socket.id != getUserLogin.socketId) {
         const filterActiveUser = activeUsers.filter(user => user.userId != userLoginId)
         filterActiveUser.push({
           userId: userLoginId,
           socketId: socket.id
         })
-  
+
         activeUsers = filterActiveUser
       }
     }
 
     console.log(activeUsers)
+
     io.emit('get-users', activeUsers)
   })
 
   // Send Message
   socket.on('send-message', (data) => {
     const { receiverId, message, senderId } = data
+    console.log(data)
     const user = activeUsers.find(user => user.userId == receiverId)
 
-    console.log(user)
-
-    if(user) {
-      console.log('tes')
+    if (user) {
+      console.log(user)
       io.to(user.socketId).emit(`receive-data`, {
-          text: message,
-          senderId
+        text: message,
+        senderId
       })
     }
   })
 
-
   socket.on('disconnect', (userId) => {
-    activeUsers = activeUsers.filter(activeUser => activeUser.id != userId)
+    activeUsers = activeUsers.filter(activeUser => activeUser.userId != userId)
+  })
+
+  socket.on('logout', ({ userId }) => {
+    activeUsers = activeUsers.filter(activeUser => activeUser.userId != userId)
+    io.emit('get-users', activeUsers)
   })
 });
 
 
+
 httpServer.listen(3000, () => {
   console.log(`server running on port 3000`)
-});
+}); 
